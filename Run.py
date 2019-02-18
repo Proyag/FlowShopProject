2665a2826d99c80885342d18e05d5991bf83ccd4
Proyag <proyag.pal@gmail.com>
2019-02-18 17:44:29 +0100

    Option to run a specific benchmark rather than all of them in Run.py

--- Run.py
+++ Run.py
@@ -10,25 +10,38 @@ taillard_file = ox.load_workbook(benchmark_file)  # Load workbook
 taillard = taillard_file.get_sheet_by_name('Sheet1')  # Load sheet
 
 
+def run_main(problem):
+    jobs, machines, timeseed, prev_best_makespan = read_problem(taillard, problem)
+
+    best_sequence, best_makespan = \
+        solve_benchmark_problem(problem, jobs, machines, timeseed, show_matrix=False)
+
+    best_sequence = list(map(int, best_sequence))
+    best_sequence = list(map(str, best_sequence))
+    best_sequence = ', '.join(best_sequence)
+
+    print("Jobs: ", jobs, "\nMachines: ", machines, "\nTimeseed: ", timeseed, "\nOptimal sequence: ", best_sequence, "\nOptimal makespan: ", best_makespan)
+
+    if best_makespan < prev_best_makespan:
+        write_new_best(problem, best_sequence, best_makespan,
+                    taillard, taillard_file, benchmark_file=benchmark_file)
+
 if __name__ == '__main__':
     # To make command line print complete nd arrays.
     np.set_printoptions(threshold=sys.maxsize)
-    # Run for entire 120 problem range
-    for problem in range(0, 120):
-        jobs, machines, timeseed, prev_best_makespan = read_problem(taillard, problem)
 
-        best_sequence, best_makespan = \
-            solve_benchmark_problem(problem, jobs, machines, timeseed, show_matrix=False)
-
-        best_sequence = list(map(int, best_sequence))
-        best_sequence = list(map(str, best_sequence))
-        best_sequence = ', '.join(best_sequence)
-
-        print("Jobs: ", jobs, "\nMachines: ", machines, "\nTimeseed: ", timeseed, "\nOptimal sequence: ", best_sequence, "\nOptimal makespan: ", best_makespan)
-
-        if best_makespan <= prev_best_makespan:
-            write_new_best(problem, best_sequence, best_makespan,
-                           taillard, taillard_file, benchmark_file=benchmark_file)
-            taillard.cell(row=problem + 3, column=6).value = best_makespan
-            taillard.cell(row=problem + 3, column=7).value = best_sequence
-            taillard_file.save('Makespans.xlsx')
+    if len(sys.argv) == 1:
+        # Run for entire 120 problem range
+        print("No argument given; running all benchmarks in sequence")
+        for problem in range(0, 120):
+            run_main(problem)
+    else:
+        try:
+            problem = int(sys.argv[1])
+        except ValueError:
+            print("Invalid argument - only argument should be a problem number (0-119)")
+            exit(1)
+        if not 0 <= problem < 120:
+            print("There are only 120 problems (numbered 0-119)")
+            exit(1)
+        run_main(problem)
